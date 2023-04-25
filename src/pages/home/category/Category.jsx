c29bc0b52ce3b5e5dc05cccbf625708874f1ef51
lolaaskerova <lolaaskrova2@gmail.com>
2023-04-25 21:37:38 +0400

    category part

---
+++ src/pages/home/category/Category.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import "./Category.scss";
+import cacti from "../../../assets/images/cacti.jpg";
+import plant from "../../../assets/images/plant.jpg";
+const Category = () => {
+  return (
+    <div className="container category">
+      <div className="left-side">
+        <div className="img">
+          <img src={cacti} alt="img" />
+        </div>
+        <div className="title">
+          <h4>Cacti</h4>
+          <h5>4 products</h5>
+        </div>
+      </div>
+      <div className="right-side">
+        <div className="img">
+          <img src={plant} alt="img" />
+          <div className="title">
+            <h4>Plants</h4>
+            <h5>4 products</h5>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Category;
