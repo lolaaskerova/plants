9566ed65616a5afeb1fe67c81dd66cc068d5cd9a
lolaaskerova <lolaaskrova2@gmail.com>
2023-04-25 21:04:53 +0400

    story section

---
+++ src/pages/home/ourstory/OurStory.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "./OurStory.scss";
+const OurStory = () => {
+  return (
+    <div className="container story">
+      <div className="titles">
+        <h4>Our story</h4>
+        <h1>
+          We are a family owned, retail and wholesale plant nursery, located in
+          New York.
+        </h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum
+          suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan
+          lacus vel facilisis.
+        </p>
+        <button>read more</button>
+      </div>
+    </div>
+  );
+};
+
+export default OurStory;
